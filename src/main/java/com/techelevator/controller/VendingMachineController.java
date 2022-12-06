dfddcf0f93e3e86ea14887323ceb246a018d18c0
Jonathan Ketner <chariotrunner@gmail.com>
2022-12-06 15:33:35 -0800

    Moved Controller out of View

--- src/main/java/com/techelevator/controller/VendingMachineController.java
+++ src/main/java/com/techelevator/controller/VendingMachineController.java
@@ -1,4 +1,34 @@
 package com.techelevator.controller;
 
+
+import com.techelevator.VendingMachineCLI;
+
+import static com.techelevator.VendingMachineCLI.MAIN_MENU_OPTION_DISPLAY_ITEMS;
+import static com.techelevator.VendingMachineCLI.MAIN_MENU_OPTION_PURCHASE;
+
+
 public class VendingMachineController {
+    VendingMachineCLI vendingMachineCLI;
+
+    public VendingMachineController(VendingMachineCLI vendingMachineCLI) {
+        this.vendingMachineCLI = vendingMachineCLI;
+    }
+
+    public void run() {
+        while (true) {
+            String choice = vendingMachineCLI.getChoice();
+
+            if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
+                // display vending machine items
+            } else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+                // do purchase
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        VendingMachineCLI vendingMachineCLI = new VendingMachineCLI();
+        VendingMachineController controller = new VendingMachineController(vendingMachineCLI);
+        controller.run();
+    }
 }

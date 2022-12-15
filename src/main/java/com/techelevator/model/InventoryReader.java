8adda22c2d1d330157090bf28484df9d8283b626
Muhammad Jahanzeb Mukhtar <jzabii.185@gmail.com>
2022-12-14 22:29:19 -0800

    Recorrection of Inventory_Reader Class

--- src/main/java/com/techelevator/model/InventoryReader.java
+++ src/main/java/com/techelevator/model/InventoryReader.java
@@ -7,17 +7,49 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class InventoryReader{
-  public static List<String> processFile()  {
-    List<String> inventoryReader=new ArrayList<String>();
+  public static List<Item> processFile()  {
+    List<Item> inventoryReader=new ArrayList<Item>();
+    StringTokenizer token=null;
+    String name="";
+    String type="";
+    String location="";
+    double cost=0;
+
 
     try{ File inventryReaderFile=new File("vendingmachine.csv");
       Scanner sc=new Scanner(inventryReaderFile);
       while (sc.hasNext()){
-        for (int i=0;i<=inventoryReader.size();i++){
-          System.out.println(inventoryReader.get(i).split("|"));
-        }
+         token=new StringTokenizer(sc.nextLine(),"|");
+         name=token.nextToken();
+         type=token.nextToken();
+         location=token.nextToken();
+         cost=Double.parseDouble(token.nextToken());
+         Item newItem=new Item() {
+           @Override
+           public String getName() {
+             return this.getName();
+           }
+
+           @Override
+           public Money getCost() {
+             return this.getCost();
+           }
+
+           @Override
+           public String dispenseMessage() {
+             return this.dispenseMessage();
+           }
+
+           @Override
+           public String getSlot() {
+             return this.getSlot();
+           }
+
+         };
+     inventoryReader.add(newItem);
 
       }
     }

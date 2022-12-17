15b698cd7cd661ee90d8e5b4423bae07311ec8f7
OriAttey <52975485+OriAttey@users.noreply.github.com>
2022-12-17 16:37:51 -0600

    commiting vending Machine

--- src/main/java/com/techelevator/model/VendingMachine.java
+++ src/main/java/com/techelevator/model/VendingMachine.java
@@ -1,30 +1,57 @@
 package com.techelevator.model;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class VendingMachine {
 
-    Money balance;
-    Map<Item, Integer> inventory = new HashMap<>();
+   private Money balance;
+    private Map<Item, Integer> inventory;
 
-    private void feedMoney(int money) {
+    public VendingMachine(List<Item> items){
+        inventory = new HashMap<>();
+        for(Item i : items){
+            inventory.put(i, 5);
+        }
+    }
+
+    public Transaction feedMoney(Money amountTOAdd) {
+        balance.add(amountTOAdd);
+        Transaction currentTran = new Transaction("FEED MONEY:",amountTOAdd , balance,"");
+        return currentTran;
+    }
+
+    public Transaction purchaseItem(Item item) throws InvalidTransactionException {
+        int clientBalance = balance.getAmount();
+        int itemCost = item.getCost().getAmount();
+        int inventoryValue = 0;
+        if(clientBalance >= itemCost && inventory.get(item) > 0){
+            balance.subtract(item.getCost());
+            // reduce the value in the inventory by 1
+
+            inventory.put(item, inventoryValue); // updating the value
+            return new Transaction(item.getName() + " " + item.getSlot(), item.getCost(), balance, item.dispenseMessage());
+        } else{
+            throw new InvalidTransactionException();
+        }
+    }
+
+    public Map<Item, Integer> getItems(){
+
+        return inventory;
+    }
+
+    public Transaction finishTransaction() throws InvalidTransactionException{
+        // String command, Money amount, Money finalBalance, String dispenseMessage
+       Money lastBalance = new Money();// the default constructor will set the balance value to zero
+        Transaction lastTransaction = new Transaction("GIVE CHANGE:",balance,lastBalance,"");
+        balance = lastBalance;
+
+        return lastTransaction;
 
     }
 
-    private void purchaseItem(Item item) throws InvalidTransactionException {
-
-    }
-
-    private List<String> getItems(){
-
-        return null;
-    }
-
-    private void finishTransaction() throws InvalidTransactionException{
-
-    }
 
 
 

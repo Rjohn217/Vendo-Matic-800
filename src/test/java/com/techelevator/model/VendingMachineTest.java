223781fa00f64bcf902f9a23cb6e5292e5919f41
OriAttey <52975485+OriAttey@users.noreply.github.com>
2022-12-17 16:47:16 -0600

    Merging branches

--- src/test/java/com/techelevator/model/VendingMachineTest.java
+++ src/test/java/com/techelevator/model/VendingMachineTest.java
@@ -1,2 +1,57 @@
-package com.techelevator.model;public class VendingMachineTest {
+package com.techelevator.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+public class VendingMachineTest extends TestCase {
+    public void testFeedMoney() {
+
+        Money moneyToAdd = new Money(5);
+        Money balance = new Money(0);
+        Transaction result = new Transaction("FEED MONEY", moneyToAdd, balance, "");
+
+        Assert.assertEquals(FEED MONEY 5 balance , result);
+    }
+
+    public void testFeedPurchaseItem() {
+        @Test
+        public void testPurchaseItem() throws InvalidTransactionException {
+
+        }
+
+            Item sampleItem = new Item();
+
+
+
+    }
+
+    public void testgetItems() {
+    List<String> myItems = new ArrayList<>();
+    Map <Item, Integer> sampleMap = new HashMap<>();
+    sampleMap.put(Beverage, 1);
+    sampleMap.put(Candy, 2);
+    sampleMap.put(Chip, 3);
+    sampleMap.put(Gum, 4);
+
+        for(Map.Entry<Item, Integer> entry : inventory.entrySet()){
+            myItems.add(entry.getKey().getName());
+        }
+
+        Assert.assertEquals("Beverage, Candy, Chip, Gum" , myItems);
+
+
+    }
+
+
+    public void testFinishTransaction() {
+        Money balance = new Money();
+        Transaction result = new Transaction();
+        Assert.assertEquals("left over balance" , result.getCommand("left over balance"));
+    }
+
+
 }
